ff3765b6f2ebf57b726e21ea64aa19e810aacebb
uriarte <uriarte0505@gmail.com>
2021-01-18 22:39:12 -0300

    Start timer when a character is typed into the text box

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-const testWrapper = document.querySelector(".test-wrapper");
-const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
-const resetButton = document.querySelector("#reset");
-const theTimer = document.querySelector(".timer");
+const testWrapper = document.querySelector(".test-wrapper")
+const testArea = document.querySelector("#test-area")
+const originText = document.querySelector("#origin-text p").innerHTML
+const resetButton = document.querySelector("#reset")
+const theTimer = document.querySelector(".timer")
 
+var timer = [0, 0, 0, 0]
 
-// Adiciona zero inicial aos números <= 9 (apenas para estética):
+// Adiciona zero inicial aos numeros <= 9
 
+// Executa um timer padrão de minuto / segundo / centesimo
 
-// Executa um timer padrão de minuto / segundo / centésimos:
+function runTimer(){
+    let currentTimer = timer[0] + ":" + timer[1] + ":" + timer[2]  
+    theTimer.innerHTML = currentTimer
+    timer[3]++
 
+    timer[0] = Math.floor((timer[3]/100)/60)
+    timer[1] = Math.floor((timer[3]/100)) - (timer[0] * 60)
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000))
 
-// Verifica se texto digitado com o fornecido na página:
-function spellCheck() { 
-    let textEntered = testArea.value;
-    console.log(textEntered);
 }
 
-// Inicia o cronômetro:
+// Verifica o texto digitado com o fornecido na pasta
+
+function spellCheck(){
+    let textEntered = testArea.value
+    console.log(textEntered)
+}
+
+// Inicia o cronometro
+
 function start(){
-    let textEnteredLength = testArea.value.length;
-    console.log(textEnteredLength);
+    let textEnteredLength = testArea.value.length
+    //console.log(textEnteredLength)
+    if (textEnteredLength === 0){
+        setInterval(runTimer, 10)
+
+    }
 }
 
-// Função de recomeçar:
-function reset() {
-    console.log("O botão de recomeçar foi clicado.");
+// Recomecar o cronometro
+
+function reset(){
+    console.log("botao pressionado")
 }
 
-// Listeners de eventos para entrada de teclado e o botão de recomeçar:
-testArea.addEventListener("keypress", start, false);
-testArea.addEventListener("keyup", spellCheck, false);
-resetButton.addEventListener("click", reset, false);
+// Executa um timer padrão de minuto / segundo / centesimo
 
+// Listener de eventos para entrada de teclado e o botao de recomecar
 
-
-
-
+testArea.addEventListener("keypress", start, false)
+testArea.addEventListener("keyup", spellCheck, false)
+resetButton.addEventListener("click", reset, false)
